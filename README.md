02daee01759bf40504d14420600ee48ade0fc7f7
Blacksorld <dmitrry.pyrkin@gmail.com>
2026-03-02 15:34:20 +0200

    Add versions, add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+## Requirements
+
+1. [nvim>=0.11.0](https://github.com/neovim/neovim)
+2. [ripgrep](https://github.com/BurntSushi/ripgrep)
--- lua/config/plugins/lualine.lua
+++ lua/config/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-lualine/lualine.nvim',
     branch = 'master',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons', version = '*' },
     config = function ()
         require('lualine').setup({})
     end
